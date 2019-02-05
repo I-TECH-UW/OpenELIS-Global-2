6d925f6090c23d4e29bacb8953dc2fbb0a3fdce1
Caleb S <caleblane40@gmail.com>
2019-02-04 17:11:20 -0800

    migrate analyzer test names

---
+++ src/main/java/spring/mine/analyzerimport/controller/AnalyzerTestNameController.java
@@ -0,0 +1,251 @@
+package spring.mine.analyzerimport.controller;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.validator.GenericValidator;
+import org.apache.struts.Globals;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import spring.mine.analyzerimport.form.AnalyzerTestNameForm;
+import spring.mine.common.controller.BaseController;
+import spring.mine.common.form.BaseForm;
+import spring.mine.common.validator.BaseErrors;
+import us.mn.state.health.lims.analyzer.dao.AnalyzerDAO;
+import us.mn.state.health.lims.analyzer.daoimpl.AnalyzerDAOImpl;
+import us.mn.state.health.lims.analyzer.valueholder.Analyzer;
+import us.mn.state.health.lims.analyzerimport.dao.AnalyzerTestMappingDAO;
+import us.mn.state.health.lims.analyzerimport.daoimpl.AnalyzerTestMappingDAOImpl;
+import us.mn.state.health.lims.analyzerimport.util.AnalyzerTestNameCache;
+import us.mn.state.health.lims.analyzerimport.valueholder.AnalyzerTestMapping;
+import us.mn.state.health.lims.common.exception.LIMSRuntimeException;
+import us.mn.state.health.lims.hibernate.HibernateUtil;
+import us.mn.state.health.lims.test.dao.TestDAO;
+import us.mn.state.health.lims.test.daoimpl.TestDAOImpl;
+import us.mn.state.health.lims.test.valueholder.Test;
+
+@Controller
+@SessionAttributes("form")
+public class AnalyzerTestNameController extends BaseController {
+
+	@ModelAttribute("form")
+	public AnalyzerTestNameForm initForm() {
+		return new AnalyzerTestNameForm();
+	}
+
+	@RequestMapping(value = "/AnalyzerTestName", method = RequestMethod.POST)
+	public ModelAndView showAnalyzerTestName(HttpServletRequest request,
+			@ModelAttribute("form") AnalyzerTestNameForm form)
+			throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		String forward = FWD_SUCCESS;
+		if (form == null) {
+			form = new AnalyzerTestNameForm();
+		}
+		form.setFormAction("");
+		BaseErrors errors = new BaseErrors();
+		if (form.getErrors() != null) {
+			errors = (BaseErrors) form.getErrors();
+		}
+		ModelAndView mv = checkUserAndSetup(form, errors, request);
+
+		if (errors.hasErrors()) {
+			return mv;
+		}
+
+		request.setAttribute(ALLOW_EDITS_KEY, "true");
+		request.setAttribute(PREVIOUS_DISABLED, "true");
+		request.setAttribute(NEXT_DISABLED, "true");
+
+		List<Analyzer> analyzerList = getAllAnalyzers();
+		List<Test> testList = getAllTests();
+
+		PropertyUtils.setProperty(form, "analyzerList", analyzerList);
+		PropertyUtils.setProperty(form, "testList", testList);
+
+		String id = request.getParameter("selectedIDs");
+		if (id != null) {
+			String[] splitId = id.split("#");
+			PropertyUtils.setProperty(form, "analyzerTestName", splitId[1]);
+			PropertyUtils.setProperty(form, "testId", splitId[2]);
+			PropertyUtils.setProperty(form, "analyzerId", splitId[0]);
+		}
+
+		return findForward(forward, form);
+	}
+
+	private List<Analyzer> getAllAnalyzers() {
+		AnalyzerDAO analyzerDAO = new AnalyzerDAOImpl();
+		return analyzerDAO.getAllAnalyzers();
+	}
+
+	private List<Test> getAllTests() {
+		TestDAO testDAO = new TestDAOImpl();
+		return testDAO.getAllActiveTests(false);
+	}
+
+	@RequestMapping(value = "/UpdateAnalyzerTestName", method = RequestMethod.POST)
+	public ModelAndView showUpdateAnalyzerTestName(HttpServletRequest request,
+			@ModelAttribute("form") AnalyzerTestNameForm form) {
+		String forward = FWD_SUCCESS;
+		if (form == null) {
+			form = new AnalyzerTestNameForm();
+		}
+		form.setFormAction("");
+		BaseErrors errors = new BaseErrors();
+		if (form.getErrors() != null) {
+			errors = (BaseErrors) form.getErrors();
+		}
+		ModelAndView mv = checkUserAndSetup(form, errors, request);
+
+		if (errors.hasErrors()) {
+			return mv;
+		}
+
+		request.setAttribute(ALLOW_EDITS_KEY, "true");
+		request.setAttribute(PREVIOUS_DISABLED, "false");
+		request.setAttribute(NEXT_DISABLED, "false");
+
+		forward = validateAndUpdateAnalyzerTestName(request, form);
+		return findForward(forward, form);
+	}
+
+	public String validateAndUpdateAnalyzerTestName(HttpServletRequest request, BaseForm dynaForm) {
+		String forward = FWD_SUCCESS_INSERT;
+		String analyzerId = dynaForm.getString("analyzerId");
+		String testId = dynaForm.getString("testId");
+		String analyzerTestName = dynaForm.getString("analyzerTestName");
+		boolean newMapping = "0".equals(request.getParameter("ID"));
+
+		Errors errors = new BaseErrors();
+
+		AnalyzerTestMapping analyzerTestNameMapping = validateAnalyzerAndTestName(analyzerId, analyzerTestName, testId,
+				errors, newMapping);
+
+		if (errors.hasErrors()) {
+			saveErrors(errors);
+			return FWD_FAIL_INSERT;
+		}
+
+		if (newMapping) {
+			analyzerTestNameMapping = new AnalyzerTestMapping();
+			analyzerTestNameMapping.setAnalyzerId(analyzerId);
+			analyzerTestNameMapping.setAnalyzerTestName(analyzerTestName);
+			analyzerTestNameMapping.setTestId(testId);
+		}
+
+		AnalyzerTestMappingDAO mappingDAO = new AnalyzerTestMappingDAOImpl();
+
+		Transaction tx = HibernateUtil.getSession().beginTransaction();
+
+		try {
+			if (newMapping) {
+				mappingDAO.insertData(analyzerTestNameMapping, currentUserId);
+			} else {
+				mappingDAO.updateMapping(analyzerTestNameMapping, currentUserId);
+			}
+
+		} catch (LIMSRuntimeException lre) {
+			tx.rollback();
+
+			String errorMsg = null;
+			if (lre.getException() instanceof org.hibernate.StaleObjectStateException) {
+				errorMsg = "errors.OptimisticLockException";
+			} else {
+				errorMsg = "errors.UpdateException";
+			}
+
+			persisteError(request, errorMsg);
+
+			disableNavigationButtons(request);
+			forward = FWD_FAIL;
+		} finally {
+			if (!tx.wasRolledBack()) {
+				tx.commit();
+			}
+			HibernateUtil.closeSession();
+		}
+
+		AnalyzerTestNameCache.instance().reloadCache();
+
+		return forward;
+	}
+
+	private AnalyzerTestMapping validateAnalyzerAndTestName(String analyzerId, String analyzerTestName, String testId,
+			Errors errors, boolean newMapping) {
+		// This is not very efficient but this is a very low usage action
+		if (GenericValidator.isBlankOrNull(analyzerId) || GenericValidator.isBlankOrNull(analyzerTestName)
+				|| GenericValidator.isBlankOrNull(testId)) {
+			errors.reject("error.all.required");
+			return null;
+		}
+
+		AnalyzerTestMapping existingMapping = null;
+		AnalyzerTestMappingDAO mappingDAO = new AnalyzerTestMappingDAOImpl();
+		List<AnalyzerTestMapping> testMappingList = mappingDAO.getAllAnalyzerTestMappings();
+		for (AnalyzerTestMapping testMapping : testMappingList) {
+			if (analyzerId.equals(testMapping.getAnalyzerId())
+					&& analyzerTestName.equals(testMapping.getAnalyzerTestName())) {
+				if (newMapping) {
+					errors.reject("error.analyzer.test.name.duplicate");
+					return null;
+				} else {
+					existingMapping = testMapping;
+					testMapping.setTestId(testId);
+					break;
+				}
+
+			}
+		}
+
+		return existingMapping;
+	}
+
+	private void persisteError(HttpServletRequest request, String errorMsg) {
+		Errors errors;
+		errors = new BaseErrors();
+
+		errors.reject(errorMsg);
+		saveErrors(errors);
+		request.setAttribute(Globals.ERROR_KEY, errors);
+	}
+
+	private void disableNavigationButtons(HttpServletRequest request) {
+		request.setAttribute(PREVIOUS_DISABLED, TRUE);
+		request.setAttribute(NEXT_DISABLED, TRUE);
+	}
+
+	@Override
+	protected ModelAndView findLocalForward(String forward, BaseForm form) {
+		if ("success".equals(forward)) {
+			return new ModelAndView("analyzerTestNameDefinition", "form", form);
+		} else if ("fail".equals(forward)) {
+			return new ModelAndView("masterListsPageDefinition", "form", form);
+		} else if (FWD_SUCCESS_INSERT.equals(forward)) {
+			return new ModelAndView("redirect:/AnalyzerTestNameMenu.do", "form", form);
+		} else if (FWD_FAIL_INSERT.equals(forward)) {
+			return new ModelAndView("redirect:/AnalyzerTestName.do", "form", form);
+		} else {
+			return new ModelAndView("PageNotFound");
+		}
+	}
+
+	@Override
+	protected String getPageTitleKey() {
+		return "analyzerTestName.browse.title";
+	}
+
+	@Override
+	protected String getPageSubtitleKey() {
+		return "analyzerTestName.browse.title";
+	}
+}
---
+++ src/main/java/spring/mine/analyzerimport/controller/AnalyzerTestNameMenuController.java
@@ -0,0 +1,226 @@
+package spring.mine.analyzerimport.controller;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.Globals;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import spring.mine.analyzerimport.form.AnalyzerTestNameMenuForm;
+import spring.mine.common.controller.BaseMenuController;
+import spring.mine.common.form.BaseForm;
+import spring.mine.common.form.MenuForm;
+import spring.mine.common.validator.BaseErrors;
+import us.mn.state.health.lims.analyzer.dao.AnalyzerDAO;
+import us.mn.state.health.lims.analyzer.daoimpl.AnalyzerDAOImpl;
+import us.mn.state.health.lims.analyzer.valueholder.Analyzer;
+import us.mn.state.health.lims.analyzerimport.action.beans.NamedAnalyzerTestMapping;
+import us.mn.state.health.lims.analyzerimport.dao.AnalyzerTestMappingDAO;
+import us.mn.state.health.lims.analyzerimport.daoimpl.AnalyzerTestMappingDAOImpl;
+import us.mn.state.health.lims.analyzerimport.util.AnalyzerTestNameCache;
+import us.mn.state.health.lims.analyzerimport.util.MappedTestName;
+import us.mn.state.health.lims.analyzerimport.valueholder.AnalyzerTestMapping;
+import us.mn.state.health.lims.common.exception.LIMSRuntimeException;
+import us.mn.state.health.lims.hibernate.HibernateUtil;
+
+@Controller
+public class AnalyzerTestNameMenuController extends BaseMenuController {
+
+	private static final int ANALYZER_NAME = 0;
+	private static final int ANALYZER_TEST = 1;
+
+	@RequestMapping(value = "/AnalyzerTestNameMenu", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView showAnalyzerTestNameMenu(HttpServletRequest request,
+			@ModelAttribute("form") AnalyzerTestNameMenuForm form)
+			throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		String forward = FWD_SUCCESS;
+		if (form == null) {
+			form = new AnalyzerTestNameMenuForm();
+		}
+		form.setFormAction("");
+		BaseErrors errors = new BaseErrors();
+		if (form.getErrors() != null) {
+			errors = (BaseErrors) form.getErrors();
+		}
+		ModelAndView mv = checkUserAndSetup(form, errors, request);
+
+		if (errors.hasErrors()) {
+			return mv;
+		}
+
+		return performMenuAction(form, request);
+	}
+
+	@Override
+	@SuppressWarnings("rawtypes")
+	protected List createMenuList(MenuForm form, HttpServletRequest request) throws Exception {
+
+		request.setAttribute("menuDefinition", "AnalyzerTestNameMenuDefinition");
+
+		String stringStartingRecNo = (String) request.getAttribute("startingRecNo");
+		int startingRecNo = Integer.parseInt(stringStartingRecNo);
+
+		List<NamedAnalyzerTestMapping> mappedTestNameList = new ArrayList<>();
+		List<String> analyzerList = AnalyzerTestNameCache.instance().getAnalyzerNames();
+		AnalyzerDAO analyzerDAO = new AnalyzerDAOImpl();
+		Analyzer analyzer = new Analyzer();
+
+		for (String analyzerName : analyzerList) {
+			Collection<MappedTestName> mappedTestNames = AnalyzerTestNameCache.instance()
+					.getMappedTestsForAnalyzer(analyzerName).values();
+			if (mappedTestNames.size() > 0) {
+				analyzer.setId(((MappedTestName) mappedTestNames.toArray()[0]).getAnalyzerId());
+				analyzer = analyzerDAO.getAnalyzerById(analyzer);
+				mappedTestNameList.addAll(convertedToNamedList(mappedTestNames, analyzer.getName()));
+			}
+		}
+
+		setDisplayPageBounds(request, mappedTestNameList.size(), startingRecNo);
+
+		return mappedTestNameList.subList(Math.min(mappedTestNameList.size(), startingRecNo - 1),
+				Math.min(mappedTestNameList.size(), startingRecNo + getPageSize()));
+
+		// return mappedTestNameList;
+	}
+
+	private List<NamedAnalyzerTestMapping> convertedToNamedList(Collection<MappedTestName> mappedTestNameList,
+			String analyzerName) {
+		List<NamedAnalyzerTestMapping> namedMappingList = new ArrayList<>();
+
+		for (MappedTestName test : mappedTestNameList) {
+			NamedAnalyzerTestMapping namedMapping = new NamedAnalyzerTestMapping();
+			namedMapping.setActualTestName(test.getOpenElisTestName());
+			namedMapping.setAnalyzerTestName(test.getAnalyzerTestName());
+			namedMapping.setAnalyzerName(analyzerName);
+
+			namedMappingList.add(namedMapping);
+		}
+
+		return namedMappingList;
+	}
+
+	private void setDisplayPageBounds(HttpServletRequest request, int listSize, int startingRecNo)
+			throws LIMSRuntimeException {
+		request.setAttribute(MENU_TOTAL_RECORDS, String.valueOf(listSize));
+		request.setAttribute(MENU_FROM_RECORD, String.valueOf(startingRecNo));
+
+		int numOfRecs = 0;
+		if (listSize != 0) {
+			numOfRecs = Math.min(listSize, getPageSize());
+
+			numOfRecs--;
+		}
+
+		int endingRecNo = startingRecNo + numOfRecs;
+		request.setAttribute(MENU_TO_RECORD, String.valueOf(endingRecNo));
+	}
+
+	@Override
+	protected String getDeactivateDisabled() {
+		return "false";
+	}
+
+	@Override
+	protected String getEditDisabled() {
+		return "true";
+	}
+
+	@RequestMapping(value = "/DeleteAnalyzerTestName", method = RequestMethod.POST)
+	public ModelAndView showDeleteAnalyzerTestName(HttpServletRequest request,
+			@ModelAttribute("form") AnalyzerTestNameMenuForm form) {
+		String forward = FWD_SUCCESS_DELETE;
+		if (form == null) {
+			form = new AnalyzerTestNameMenuForm();
+		}
+		form.setFormAction("");
+		BaseErrors errors = new BaseErrors();
+		if (form.getErrors() != null) {
+			errors = (BaseErrors) form.getErrors();
+		}
+		ModelAndView mv = checkUserAndSetup(form, errors, request);
+
+		if (errors.hasErrors()) {
+			return mv;
+		}
+
+		String[] selectedIDs = (String[]) form.get("selectedIDs");
+
+		// String sysUserId = getSysUserId(request);
+		List<AnalyzerTestMapping> testMappingList = new ArrayList<>();
+
+		for (int i = 0; i < selectedIDs.length; i++) {
+			String[] ids = selectedIDs[i].split(NamedAnalyzerTestMapping.getUniqueIdSeperator());
+			AnalyzerTestMapping testMapping = new AnalyzerTestMapping();
+			testMapping.setAnalyzerId(AnalyzerTestNameCache.instance().getAnalyzerIdForName(ids[ANALYZER_NAME]));
+			testMapping.setAnalyzerTestName(ids[ANALYZER_TEST]);
+			testMappingList.add(testMapping);
+		}
+
+		org.hibernate.Transaction tx = HibernateUtil.getSession().beginTransaction();
+		try {
+
+			AnalyzerTestMappingDAO testMappingDAO = new AnalyzerTestMappingDAOImpl();
+			testMappingDAO.deleteData(testMappingList, currentUserId);
+
+			tx.commit();
+		} catch (LIMSRuntimeException lre) {
+			tx.rollback();
+			if (lre.getException() instanceof org.hibernate.StaleObjectStateException) {
+				errors.reject("errors.OptimisticLockException");
+			} else {
+				errors.reject("errors.DeleteException");
+			}
+			saveErrors(errors);
+			request.setAttribute(Globals.ERROR_KEY, errors);
+			forward = FWD_FAIL_DELETE;
+
+		} finally {
+			HibernateUtil.closeSession();
+		}
+		if (forward.equals(FWD_FAIL_DELETE)) {
+			return findForward(forward, form);
+		}
+
+		if (TRUE.equalsIgnoreCase(request.getParameter("close"))) {
+			forward = FWD_CLOSE;
+		}
+
+		AnalyzerTestNameCache.instance().reloadCache();
+		request.setAttribute("menuDefinition", "AnalyzerTestNameDefinition");
+
+		return findForward(forward, form);
+	}
+
+	@Override
+	protected ModelAndView findLocalForward(String forward, BaseForm form) {
+		if ("success".equals(forward)) {
+			return new ModelAndView("haitiMasterListsPageDefinition", "form", form);
+		} else if ("fail".equals(forward)) {
+			return new ModelAndView("/MasterListsPage.do", "form", form);
+		} else if (FWD_SUCCESS_DELETE.equals(forward)) {
+			return new ModelAndView("redirect:/AnalyzerTestNameMenu.do", "form", form);
+		} else if (FWD_FAIL_DELETE.equals(forward)) {
+			return new ModelAndView("redirect:/AnalyzerTestNameMenu.do", "form", form);
+		} else {
+			return new ModelAndView("PageNotFound");
+		}
+	}
+
+	@Override
+	protected String getPageTitleKey() {
+		return "analyzerTestName.browse.title";
+	}
+
+	@Override
+	protected String getPageSubtitleKey() {
+		return "analyzerTestName.browse.title";
+	}
+}
