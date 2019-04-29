ef7853a8a06834183ce599f33f4a6e4ffa3f486c
Caleb S <caleblane40@gmail.com>
2019-04-29 10:21:42 -0700

    fix blob data types

--- src/main/java/us/mn/state/health/lims/image/daoimpl/ImageDAOImpl.java
+++ src/main/java/us/mn/state/health/lims/image/daoimpl/ImageDAOImpl.java
@@ -15,11 +15,9 @@
  */
 package us.mn.state.health.lims.image.daoimpl;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.sql.SQLException;
 
 import org.hibernate.HibernateException;
 
@@ -29,91 +27,86 @@ import us.mn.state.health.lims.hibernate.HibernateUtil;
 import us.mn.state.health.lims.image.dao.ImageDAO;
 import us.mn.state.health.lims.image.valueholder.Image;
 
+public class ImageDAOImpl extends BaseDAOImpl implements ImageDAO {
 
-public class ImageDAOImpl extends BaseDAOImpl implements ImageDAO{
+	@Override
+	public String saveImage(Image image) throws LIMSRuntimeException {
+		String id = image.getId();
+		try {
+			if (id == null) {
+				id = (String) HibernateUtil.getSession().save(image);
+				closeSession();
+			} else { // this part does not seem to work so we are deleting and then adding as a work
+						// around
+				HibernateUtil.getSession().merge(image);
+				closeSession();
+				HibernateUtil.getSession().evict(image);
+				HibernateUtil.getSession().refresh(image);
+			}
+		} catch (HibernateException e) {
+			handleException(e, "saveImage");
+		}
 
-    @Override
-    public String saveImage( Image image ) throws LIMSRuntimeException{
-        String id = image.getId();
-        try{
-            if( id == null ){
-                id = ( String ) HibernateUtil.getSession().save( image );
-                closeSession();
-            }else{  //this part does not seem to work so we are deleting and then adding as a work around
-                HibernateUtil.getSession().merge( image );
-                closeSession();
-                HibernateUtil.getSession().evict( image );
-                HibernateUtil.getSession().refresh( image );
-            }
-        }catch( HibernateException e ){
-            handleException( e, "saveImage" );
-        }
+		return id;
+	}
 
-        return id;
-    }
+	@Override
+	public Image getImage(String imageId) throws LIMSRuntimeException {
+		try {
+			Image image = (Image) HibernateUtil.getSession().get(Image.class, imageId);
+			closeSession();
+			return image;
+		} catch (HibernateException e) {
+			handleException(e, "getImage");
+		}
 
-    @Override
-    public Image getImage( String imageId ) throws LIMSRuntimeException{
-        try{
-            Image image = ( Image ) HibernateUtil.getSession().get( Image.class, imageId );
-            closeSession();
-            return image;
-        }catch( HibernateException e ){
-            handleException( e, "getImage" );
-        }
+		return null;
+	}
 
-        return null;
-    }
+	@Override
+	public void deleteImage(Image image) throws LIMSRuntimeException {
+		try {
+			HibernateUtil.getSession().delete(image);
+			closeSession();
+		} catch (Exception e) {
+			handleException(e, "deleteImage");
+		}
+	}
 
-    @Override
-    public void deleteImage( Image image ) throws LIMSRuntimeException{
-        try{
-            HibernateUtil.getSession().delete( image );
-            closeSession();
-        }catch( Exception e ){
-            handleException( e, "deleteImage" );
-        }
-    }
+	@Override
+	@Deprecated
+	public ByteArrayOutputStream retrieveImageOutputStream(String id) throws LIMSRuntimeException {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(Image.MAX_MEMORY_SIZE);
+		try {
+			Image image = getImage(id);
 
-    @Override
-    public ByteArrayOutputStream retrieveImageOutputStream( String id ) throws LIMSRuntimeException{
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( Image.MAX_MEMORY_SIZE );
-        try{
-            Image image = getImage( id );
+			outputStream.write(image.getImage());
 
-            byte[] bindata = new byte[ Image.MAX_MEMORY_SIZE ];
-            int bytesread;
-            BufferedInputStream bufferedInputStream = new BufferedInputStream( image.getImage().getBinaryStream() );
-            if( ( bytesread = bufferedInputStream.read( bindata, 0, bindata.length ) ) != -1 ){
-                outputStream.write( bindata, 0, bytesread );
-            }
+		} catch (HibernateException e) {
+			handleException(e, "retrieveImageOutputStream");
+		} catch (IOException e) {
+			handleException(e, "retrieveImageOutputStream");
+		}
 
-        }catch( HibernateException e ){
-            handleException( e, "retrieveImageOutputStream" );
-        }catch( SQLException e ){
-            handleException( e, "retrieveImageOutputStream" );
-        }catch( IOException e ){
-            handleException( e, "retrieveImageOutputStream" );
-        }
+		return outputStream;
+	}
 
-        return outputStream;
-    }
+	@Override
+	@Deprecated
+	public ByteArrayInputStream retrieveImageInputStream(String id) throws LIMSRuntimeException {
+		return new ByteArrayInputStream(retrieveImageOutputStream(id).toByteArray());
+	}
 
-    @Override
-    public ByteArrayInputStream retrieveImageInputStream( String id ) throws LIMSRuntimeException{
-        return new ByteArrayInputStream( retrieveImageOutputStream( id ).toByteArray() );
-    }
+	public Image readImage(String idString) {
+		try {
+			Image image = (Image) HibernateUtil.getSession().get(Image.class, idString);
+			HibernateUtil.getSession().flush();
+			HibernateUtil.getSession().clear();
+			return image;
+		} catch (Exception e) {
+			handleException(e, "readImage");
+		}
 
-    public Image readImage(String idString) {
-        try {
-            Image image = (Image) HibernateUtil.getSession().get(Image.class, idString);
-            HibernateUtil.getSession().flush();
-            HibernateUtil.getSession().clear();
-            return image;
-        } catch (Exception e) {
-            handleException( e, "readImage" );
-        }
-
-        return null;
-    }
+		return null;
+	}
 }
