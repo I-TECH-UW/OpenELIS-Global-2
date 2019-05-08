897938cddb1d0ae8afe7c47fce5a9fac25cac931
rossumg <rossumg@uw.edu>
2019-05-08 16:37:03 -0700

    add service layer

---
+++ src/main/java/spring/mine/config/HibernateConfig.java
@@ -0,0 +1,37 @@
+package spring.mine.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+//	@ComponentScans(value = { @ComponentScan("com.howtodoinjava.demo.spring")})
+public class HibernateConfig {
+
+	@Autowired
+	private ApplicationContext context;
+
+	@Bean
+	public LocalSessionFactoryBean getSessionFactory() {
+		LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+		factoryBean.setConfigLocation(context.getResource("classpath:hibernate/hibernate.cfg.xml"));
+//		factoryBean.setAnnotatedClasses(User.class);//
+		return factoryBean;
+	}
+
+	@Bean
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory);
+		return transactionManager;
+	}
+
+}
