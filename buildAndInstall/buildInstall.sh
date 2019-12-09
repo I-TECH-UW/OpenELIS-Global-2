2c0e8b458a6395cfd5098956bda3ee322d3d552e
CalebSLane <c.steelelane@gmail.com>
2019-12-09 00:53:30 -0800

    add READMEs to directories so directories exist in github

--- buildAndInstall/buildInstall.sh
+++ buildAndInstall/buildInstall.sh
@@ -2,8 +2,8 @@
 PROGNAME=$0
 callDirectory=$PWD
 
-installerDir="~/OEInstaller"
-stagingDir="~/OEInstaller_stagingDir"
+installerDir="OEInstaller"
+stagingDir="OEInstaller_stagingDir"
 
 usage() {
   cat << EOF >&2
@@ -41,6 +41,31 @@ buildInstallDir="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 #and other important locations
 projectDir="${buildInstallDir}/.."
 liquibaseDir="${projectDir}/liquibase"
+
+echo Will build from $branch
+#cd source/openelisglobal-core
+#git checkout -- app/src/build.properties
+git checkout $branch
+if [ $? != 0 ]
+then
+    echo
+    echo "branch is not local will try to create"
+    echo 
+    
+    git checkout -b $branch origin/$branch
+
+    if [ $? != 0 ]
+	then
+	echo
+	echo "$branch not found in main repository. Check name"
+	exit 1 
+    fi
+fi
+git pull origin $branch
+
+#git rev-list HEAD | tac | nl | tail -n 1 | sed 's/\t/ hash-/g'  |sed 's/\s\{2,\}/revision-/g' > ../../version.txt 
+#cd ../..
+#sed '2!d' source/openelisglobal-core/app/src/build.properties  > build.txt
 
 if [ $runLiquibase == true ]
 then
