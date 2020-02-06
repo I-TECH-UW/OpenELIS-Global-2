d89c3b18f1172913d1af95182251abcf6a180fdd
CalebSLane <c.steelelane@gmail.com>
2020-02-05 19:41:13 -0800

    remove PluginFile example files

--- src/main/java/org/openelisglobal/plugin/dao/PluginFileDAO.java
+++
@@ -1,8 +0,0 @@
-package org.openelisglobal.plugin.dao;
-
-import org.openelisglobal.common.dao.BaseDAO;
-import org.openelisglobal.plugin.valueholder.PluginFile;
-
-public interface PluginFileDAO extends BaseDAO<PluginFile, Long> {
-
-}
