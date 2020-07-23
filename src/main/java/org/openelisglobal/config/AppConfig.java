55c6c4138333081ff313a5dd67051f7fe607f930
CalebSLane <c.steelelane@gmail.com>
2020-07-22 20:49:43 -0700

    remove cert auth in OpenELIS and speed up deployment time

--- src/main/java/org/openelisglobal/config/AppConfig.java
+++ src/main/java/org/openelisglobal/config/AppConfig.java
@@ -108,8 +108,8 @@ public class AppConfig implements WebMvcConfigurer {
         registry.addInterceptor(localeChangeInterceptor()).addPathPatterns("/**");
         registry.addInterceptor(moduleAuthenticationInterceptor).addPathPatterns("/**")
                 .excludePathPatterns(SecurityConfig.OPEN_PAGES).excludePathPatterns(SecurityConfig.RESOURCE_PAGES)
-                .excludePathPatterns(SecurityConfig.AUTH_OPEN_PAGES)
-                .excludePathPatterns(SecurityConfig.CLIENT_CERTIFICATE_PAGES);
+                .excludePathPatterns(SecurityConfig.AUTH_OPEN_PAGES);
+//                .excludePathPatterns(SecurityConfig.CLIENT_CERTIFICATE_PAGES);
         registry.addInterceptor(urlLocatedErrorsInterceptor).addPathPatterns("/**");
         registry.addInterceptor(pageAttributesInterceptor).addPathPatterns("/**");
     }
