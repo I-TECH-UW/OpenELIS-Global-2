e33f2fb2dfe0b75576ee80a97e0d1bb91f7d477a
rossumg <rossumg@uw.edu>
2020-08-18 13:52:41 -0700

    add iframe openlmis
     id  | parent_id | presentation_order |  element_id   |    action_url
    | click_action |     display_key      | tool_tip_key | new_window |
    is_active
    -----+-----------+--------------------+---------------+------------------+--------------+----------------------+--------------+------------+-----------
     185 |           |                9.5 | menu_openlmis | /OpenlmisPage.do
    |              | banner.menu.openlmis |              | f          | t

---
+++ src/main/java/org/openelisglobal/menu/controller/OpenlmisPageController.java
@@ -0,0 +1,49 @@
+package org.openelisglobal.menu.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.openelisglobal.common.controller.BaseController;
+import org.openelisglobal.common.form.MainForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class OpenlmisPageController extends BaseController {
+
+    private static final String[] ALLOWED_FIELDS = new String[] {};
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setAllowedFields(ALLOWED_FIELDS);
+    }
+
+    @RequestMapping(value = "/OpenlmisPage", method = { RequestMethod.GET, RequestMethod.POST })
+    public ModelAndView showOpenlmisPage(HttpServletRequest request) {
+
+        MainForm form = new MainForm();
+        return findForward(FWD_SUCCESS, form);
+    }
+
+    @Override
+    protected String findLocalForward(String forward) {
+        if (FWD_SUCCESS.equals(forward)) {
+            return "openlmisPageDefinition";
+        } else {
+            return "PageNotFound";
+        }
+    }
+
+    @Override
+    protected String getPageTitleKey() {
+        return null;
+    }
+
+    @Override
+    protected String getPageSubtitleKey() {
+        return null;
+    }
+}
---
+++ src/main/webapp/pages/openlmis/openlmisPage.jsp
@@ -0,0 +1,14 @@
+<%@ page import="org.openelisglobal.common.util.ConfigurationProperties,
+				org.openelisglobal.common.util.ConfigurationProperties.Property,
+				org.openelisglobal.common.util.Versioning" %>
+<%@ page isELIgnored="false" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<%@ taglib prefix="ajax" uri="/tags/ajaxtags" %>
+
+<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles"%>
+
+<br/>
+<iframe src="https://demo-v3.openlmis.org/#!/login" onload='javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));' style="height:800px;width:100%;border:none;overflow:hidden;"></iframe>
