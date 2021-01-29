2a0d351a66842cd520254ecb53bf076bacafa5fb
CalebSLane <c.steelelane@gmail.com>
2021-01-29 16:35:05 -0700

    bugfix #176710988 hide report management, not report configuration

--- src/main/java/org/openelisglobal/menu/service/AdminMenuItemServiceImpl.java
+++ src/main/java/org/openelisglobal/menu/service/AdminMenuItemServiceImpl.java
@@ -99,13 +99,18 @@ public class AdminMenuItemServiceImpl implements AdminMenuItemService, Configura
             curItem.setMessageKey("resultConfiguration.browse.title");
             adminMenuItems.add(curItem);
         }
-        
+
         // gnr: pivotal 176523804, 176372736
-        if(false) {
-        if (adminFields.useField(Field.NON_CONFORMITY_CONFIGURATION)) {
+        if (false) {
+            if (adminFields.useField(Field.NON_CONFORMITY_CONFIGURATION)) {
+                curItem = new AdminMenuItem();
+                curItem.setPath("/NonConformityConfigurationMenu.do");
+                curItem.setMessageKey("nonConformityConfiguration.browse.title");
+                adminMenuItems.add(curItem);
+            }
             curItem = new AdminMenuItem();
-            curItem.setPath("/NonConformityConfigurationMenu.do");
-            curItem.setMessageKey("nonConformityConfiguration.browse.title");
+            curItem.setPath("/ReportConfiguration.do");
+            curItem.setMessageKey("banner.menu.reportManagement");
             adminMenuItems.add(curItem);
         }
 
@@ -115,7 +120,6 @@ public class AdminMenuItemServiceImpl implements AdminMenuItemService, Configura
             curItem.setMessageKey("printedReportsConfiguration.browse.title");
             adminMenuItems.add(curItem);
         }
-        }
 
         if (adminFields.useField(Field.WORKPLAN_CONFIGURATION)) {
             curItem = new AdminMenuItem();
@@ -123,12 +127,6 @@ public class AdminMenuItemServiceImpl implements AdminMenuItemService, Configura
             curItem.setMessageKey("workplanConfiguration.browse.title");
             adminMenuItems.add(curItem);
         }
-
-
-        curItem = new AdminMenuItem();
-        curItem.setPath("/ReportConfiguration.do");
-        curItem.setMessageKey("banner.menu.reportManagement");
-        adminMenuItems.add(curItem);
 
         if (adminFields.useField(Field.TypeOfSamplePanelMenu)) {
             curItem = new AdminMenuItem();
