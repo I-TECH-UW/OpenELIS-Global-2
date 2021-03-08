d578d1cffd93b3f3621232303961315e12729cf7
CalebSLane <c.steelelane@gmail.com>
2021-03-08 15:43:15 -0800

    feature #177208682 add mauritius protocol report

--- src/main/java/org/openelisglobal/reports/controller/ReportController.java
+++ src/main/java/org/openelisglobal/reports/controller/ReportController.java
@@ -52,7 +52,7 @@ public class ReportController extends BaseController {
     private static final String[] ALLOWED_FIELDS = new String[] { "report", "reportType", "type", "accessionDirect",
             "highAccessionDirect", "patientNumberDirect", "patientUpperNumberDirect", "lowerDateRange",
             "upperDateRange", "locationCode", "projectCode", "datePeriod", "lowerMonth", "lowerYear", "upperMonth",
-            "upperYear", "selectList.selection", };
+            "upperYear", "selectList.selection", "experimentId" };
 
     @Autowired
     private ServletContext context;
