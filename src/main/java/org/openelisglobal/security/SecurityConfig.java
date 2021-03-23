5f97fada42a57a049c2dfff7994fab09ce863dbd
CalebSLane <c.steelelane@gmail.com>
2021-03-23 13:47:26 -0700

    geneXpert work

--- src/main/java/org/openelisglobal/security/SecurityConfig.java
+++ src/main/java/org/openelisglobal/security/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
     public static final String[] AUTH_OPEN_PAGES = { "/Home.do", "/Dashboard.do", "/Logout.do", "/MasterListsPage.do" };
     public static final String[] RESOURCE_PAGES = { "/css/**", "/favicon/**", "/images/**", "/documentation/**",
             "/scripts/**", "/jsp/**" };
-    public static final String[] HTTP_BASIC_SERVLET_PAGES = { "/importAnalyzer/**", "/fhir/**" };
+    public static final String[] HTTP_BASIC_SERVLET_PAGES = { "/pluginServlet/**", "/importAnalyzer/**",
+            "/fhir/**" };
 //    public static final String[] CLIENT_CERTIFICATE_PAGES = {};
 
     private static final String CONTENT_SECURITY_POLICY = "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval';"
