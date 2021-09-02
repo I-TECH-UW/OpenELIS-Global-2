8c4baf318defc9facd92018f506b1a091d4a65c3
CalebSLane <c.steelelane@gmail.com>
2021-09-01 23:52:54 -0700

    change order of patient search in provider

--- src/main/java/org/openelisglobal/common/provider/query/PatientSearchPopulateProvider.java
+++ src/main/java/org/openelisglobal/common/provider/query/PatientSearchPopulateProvider.java
@@ -79,12 +79,12 @@ public class PatientSearchPopulateProvider extends BaseQueryProvider {
         String patientKey = request.getParameter("personKey");
         StringBuilder xml = new StringBuilder();
         String result = null;
-        if (nationalId != null) {
-            result = createSearchResultXML(patientService.getPatientByNationalId(nationalId), xml);
+        if (GenericValidator.isBlankOrNull(patientKey)) {
+            result = createSearchResultXML(getPatientForID(patientKey), xml);
         } else if (externalId != null) {
             result = createSearchResultXML(patientService.getPatientByExternalId(externalId), xml);
-        } else {
-            result = createSearchResultXML(getPatientForID(patientKey), xml);
+        } else if (nationalId != null) {
+            result = createSearchResultXML(patientService.getPatientByNationalId(nationalId), xml);
         }
         if (!result.equals(VALID)) {
             result = MessageUtil.getMessage("patient.message.patientNotFound");
