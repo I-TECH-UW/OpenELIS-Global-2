9f049d18674e4570a58ded646b5a94189ca30c0a
Mutesasira Moses <mozzymutesa@gmail.com>
2021-10-14 14:01:14 +0300

    Update AppConfig.java

--- src/main/java/org/openelisglobal/config/AppConfig.java
+++ src/main/java/org/openelisglobal/config/AppConfig.java
@@ -46,7 +46,8 @@ import org.springframework.web.servlet.view.tiles3.TilesView;
 @Configuration
 @EnableJpaRepositories(basePackages = { "org.itech", "org.ozeki.sms" })
 @PropertySource("classpath:application.properties")
-@PropertySource("file:/run/secrets/common.properties")
+@PropertySource("classpath:common.properties")
+@PropertySource(value ="file:/run/secrets/common.properties", ignoreResourceNotFound = true)
 @PropertySource(value = "file:/run/secrets/extra.properties", ignoreResourceNotFound = true)
 @ComponentScan(basePackages = { "spring", "org.openelisglobal", "org.itech", "org.ozeki.sms", "oe.plugin" })
 public class AppConfig implements WebMvcConfigurer {
