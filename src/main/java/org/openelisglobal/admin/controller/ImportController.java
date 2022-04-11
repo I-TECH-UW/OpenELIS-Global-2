209f3aa4eeff443ed42065f9e8c3225cb53a337e
CalebSLane <c.steelelane@gmail.com>
2022-04-11 12:57:37 -0700

    fix circular dependency

--- src/main/java/org/openelisglobal/admin/controller/ImportController.java
+++ src/main/java/org/openelisglobal/admin/controller/ImportController.java
@@ -6,7 +6,7 @@ import org.openelisglobal.dataexchange.fhir.exception.FhirGeneralException;
 import org.openelisglobal.dataexchange.fhir.exception.FhirLocalPersistingException;
 import org.openelisglobal.organization.service.OrganizationImportService;
 import org.openelisglobal.provider.service.ProviderImportService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openelisglobal.spring.util.SpringContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,25 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/import")
 public class ImportController {
 
-    @Autowired
-    private OrganizationImportService organizationImportService;
-    @Autowired
-    private ProviderImportService providerImportService;
-
     @GetMapping(value = "/all")
     public void importAll() throws FhirLocalPersistingException, FhirGeneralException, IOException {
-        organizationImportService.importOrganizationList();
-        providerImportService.importPractitionerList();
+        SpringContext.getBean(OrganizationImportService.class).importOrganizationList();
+        SpringContext.getBean(ProviderImportService.class).importPractitionerList();
     }
 
     @GetMapping(value = "/organization")
     public void importOrganizations() throws FhirLocalPersistingException, FhirGeneralException, IOException {
-        organizationImportService.importOrganizationList();
+        SpringContext.getBean(OrganizationImportService.class).importOrganizationList();
     }
 
     @GetMapping(value = "/provider")
     public void importProviders() throws FhirLocalPersistingException, FhirGeneralException, IOException {
-        providerImportService.importPractitionerList();
+        SpringContext.getBean(ProviderImportService.class).importPractitionerList();
     }
 
 }
