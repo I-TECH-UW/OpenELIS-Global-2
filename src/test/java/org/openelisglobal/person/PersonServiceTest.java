6461cf54b7244070e131c477ca0b82200cc0bf78
mozzy11 <mozzymutesa@gmail.com>
2022-10-11 20:31:42 +0300

    add some intergration tests

--- src/test/java/org/openelisglobal/person/PersonServiceTest.java
+++ src/test/java/org/openelisglobal/person/PersonServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.openelisglobal.BaseTestConfig;
 import org.openelisglobal.patient.PatientTestConfig;
 import org.openelisglobal.person.service.PersonService;
+import org.openelisglobal.person.valueholder.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -28,6 +29,29 @@ public class PersonServiceTest {
 
     @Test
     public void getAllPerson_shouldGetAllPatients() throws Exception {
-        Assert.assertEquals(0, personService.getAllPersons().size());
+        Assert.assertEquals(1, personService.getAllPersons().size());
     }
+
+    @Test
+    public void createPerson_shouldCreateNewPerson() throws Exception {
+        String firstName = "John";
+        String lastname = "Doe";
+
+        Person pat = createPerson(firstName, lastname);
+        // save person to the DB
+        String personIdId = personService.insert(pat);
+        Person savedPerson = personService.get(personIdId);
+
+        Assert.assertEquals(1, personService.getAllPersons().size());
+        Assert.assertEquals(firstName, savedPerson.getFirstName());
+        Assert.assertEquals(lastname, savedPerson.getLastName());
+    }
+
+    private Person createPerson(String firstName, String LastName) {
+        Person person = new Person();
+        person.setFirstName(firstName);
+        person.setLastName(LastName);
+        return person;
+    }
+
 }
