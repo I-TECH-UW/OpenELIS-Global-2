25acf220ecb68e30b171ed44c07fc032e261a67b
abertnamanya <abertnamanya@gmail.com>
2023-05-02 20:16:48 +0300

    Sample Order Payload initiation

--- react-ui/src/components/addOrder/CreateOrder.js
+++ react-ui/src/components/addOrder/CreateOrder.js
@@ -1,51 +1,79 @@
 import React, {useState} from 'react'
-import {Column, Form, Grid,} from '@carbon/react';
+import {Button, Column, Form, Grid} from '@carbon/react';
 
 import AddSample from './AddSample'
 import AddOrderTopForm from "./AddOrderTopForm";
 import SearchPatientForm from "../common/SearchPatientForm";
 import CreatePatientForm from "../common/CreatePatientForm";
 import OrderResultReporting from "./OrderResultReporting";
+import {FormattedMessage} from "react-intl";
+
+import {OrderFormValues} from "../formModel/innitialValues/OrderEntryFormValues";
 
 const CreateOrder = () => {
     const [selectedPatient, setSelectedPatient] = useState({
-        id: "",
-        healthRegion: []
+        id: "", healthRegion: []
     });
+    const [orderFormValues, setOrderFormValues] = useState(OrderFormValues);
 
     const [samples, setSamples] = useState([]);
 
     const getSelectedPatient = (patient) => {
         setSelectedPatient(patient);
     }
-    return (
-        <>
-            <Grid fullWidth={true} className="gridBoundary">
-                <Column lg={16}>
-                    <Form>
-                        <AddOrderTopForm/>
-                        <br/>
-                        <AddSample setSamples={setSamples}/>
-                    </Form>
-                    <SearchPatientForm getSelectedPatient={getSelectedPatient}></SearchPatientForm>
-                    <CreatePatientForm showActionsButton={false} selectedPatient={selectedPatient}
-                                       getSelectedPatient={getSelectedPatient}/>
-                    {samples.length > 0 && <h3>Result Reporting</h3>}
-                    {
-                        samples.map((sample, index) => {
-                            if (sample.tests.length > 0) {
-                                return (
-                                    <div key={index}>
-                                        <h4>Sample {index + 1}</h4>
-                                        <OrderResultReporting selectedTests={sample.tests}/>
-                                    </div>)
-                            }
-                        })
+
+    function reportingNotifications(object) {
+        setOrderFormValues({
+            ...orderFormValues,
+            patientSMSNotificationTestIds: object.patientSMSNotificationTestIds,
+            patientEmailNotificationTestIds: object.patientEmailNotificationTestIds,
+            providerSMSNotificationTestIds: object.providerSMSNotificationTestIds,
+            providerEmailNotificationTestIds: object.providerEmailNotificationTestIds,
+        });
+    }
+
+    const handleSubmitOrderForm = (e) => {
+        e.preventDefault();
+        // console.log(samples);
+        console.log(orderFormValues);
+    }
+    return (<>
+        <Grid fullWidth={true} className="gridBoundary">
+            <Column lg={16}>
+                <Form>
+                    <AddOrderTopForm orderFormValues={orderFormValues} setOrderFormValues={setOrderFormValues}/>
+                    <br/>
+                    <AddSample setSamples={setSamples}/>
+                </Form>
+                <SearchPatientForm getSelectedPatient={getSelectedPatient}></SearchPatientForm>
+                <CreatePatientForm showActionsButton={false} selectedPatient={selectedPatient}
+                                   orderFormValues={orderFormValues} setOrderFormValues={setOrderFormValues}
+                                   getSelectedPatient={getSelectedPatient}/>
+                {samples.length > 0 && <h3>Result Reporting</h3>}
+
+                {samples.map((sample, index) => {
+                    if (sample.tests.length > 0) {
+                        return (<div key={index}>
+                            <h4>Sample {index + 1}</h4>
+                            <OrderResultReporting selectedTests={sample.tests}
+                                                  reportingNotifications={reportingNotifications}/>
+                        </div>)
                     }
-                </Column>
-            </Grid>
-        </>
-    )
+                })}
+            </Column>
+            <Column lg={6}>
+                <div>
+                    <Button type="button" id="submit" onClick={handleSubmitOrderForm}>
+                        <FormattedMessage id="label.button.save"/>
+                    </Button>
+                    <Button id="clear" kind='danger'>
+                        <FormattedMessage id="label.button.clear"/>
+                    </Button>
+                </div>
+            </Column>
+
+        </Grid>
+    </>)
 }
 
 export default CreateOrder;
--- react-ui/src/components/addOrder/OrderItemsTableRow.js
+++ react-ui/src/components/addOrder/OrderItemsTableRow.js
@@ -1,69 +1,20 @@
-import {
-    Checkbox,
-    DatePicker,
-    DatePickerInput,
-    Link,
-    RadioButton,
-    Select,
-    SelectItem,
-    TextArea,
-    TextInput,
-    TimePicker
-} from "@carbon/react";
-import React, {useContext, useState} from "react";
+import {Link, RadioButton} from "@carbon/react";
+import React, {useContext, useEffect, useState} from "react";
 import {NotificationKinds} from "../common/CustomNotification";
 import {NotificationContext} from "../layout/Layout";
 import {FormattedMessage} from "react-intl";
-
-const Sequence = (index) => {
-    return (<TextInput id={`sequence_${index}`} value={index + 1} style={{width: "50px"}}
-                       labelText=""/>);
-}
+import CustomTextInput from "../common/CustomTextInput";
+import CustomDatePicker from "../common/CustomDatePicker";
+import CustomSelect from "../common/CustomSelect";
+import CustomTimePicker from "../common/CustomTimePicker";
+import CustomCheckBox from "../common/CustomCheckBox";
 
 const Sample_Type = (index, name) => {
     return (<span id={`typeId_${index}`}>{name}</span>);
 }
 
-
-const CollectionDate = (index,defaultDate) => {
-    return (<DatePicker dateFormat="m/d/Y" datePickerType="single"  value={defaultDate}>
-        <DatePickerInput id={`collectionDate_` + index} placeholder="dd/mm/yyyy" type="text" labelText=""
-                         style={{width: "140px"}}/>
-    </DatePicker>);
-}
-
 const SampleSelect = (index) => {
     return (<RadioButton checked={true} id={`select_${index}`} value={index} labelText=""/>);
-}
-
-
-const CollectionTime = (index,defaultTime) => {
-    return (<TimePicker id={`collectionTime_${index}`} value={defaultTime}/>);
-}
-
-const Collector = (index) => {
-    return (<TextInput id={`collector${index}`} style={{width: "120px"}} labelText=""/>);
-}
-
-
-const RejectReasons = (index, rejectSampleReasons, rejectionReasonsDisabled) => {
-    return (<Select
-        disabled={rejectionReasonsDisabled}
-        id={`rejectedReasonId_${index}`}
-        labelText="">
-        <SelectItem
-            text=""
-            value=""
-        />
-        {rejectSampleReasons.map(reason => {
-            return (<SelectItem
-                key={reason.id}
-                text={reason.value}
-                value={reason.id}
-            />)
-        })}
-
-    </Select>);
 }
 
 const RemoveSample = (index, handleRemoveSampleTest) => {
@@ -75,11 +26,53 @@ const RemoveSample = (index, handleRemoveSampleTest) => {
     return (<Link href="#" id={`removeButton_${index}`} onClick={(e) => handleRemoveSample(e)}>Remove Sample</Link>);
 }
 
+export const TableSampleTableRows = (index, selectedSampleType, rejectSampleReasons, selectedTests, updateSampleXml, handleRemoveSampleTest) => {
+    const {setNotificationVisible, setNotificationBody} = useContext(NotificationContext);
+    const {id, name} = selectedSampleType;
+    const [rejectionReasonsDisabled, setRejectionReasonsDisabled] = useState(true);
+    const defaultSelect = {id: "", value: ""};
+    const [sampleXml, setSampleXml] = useState({
+        collectionDate: "",
+        collector: "",
+        rejected: false,
+        rejectionReason: "",
+        collectionTime: ""
+    });
 
-const Reject = (index, rejectionReasonsDisabled, setRejectionReasonsDisabled) => {
-    const { setNotificationVisible, setNotificationBody } = useContext(NotificationContext);
-    function handleRejection(e) {
-        if (e.currentTarget.checked) {
+    function handleCollectionDate(date) {
+        setSampleXml({
+            ...sampleXml,
+            collectionDate: date
+        });
+    }
+
+    function handleCollector(value) {
+        setSampleXml({
+            ...sampleXml,
+            collector: value
+        });
+    }
+
+    function handleReasons(value) {
+        setSampleXml({
+            ...sampleXml,
+            rejectionReason: value
+        });
+    }
+
+    function handleCollectionTime(time) {
+        setSampleXml({
+            ...sampleXml,
+            collectionTime: time
+        });
+    }
+
+    useEffect(() => {
+        updateSampleXml(sampleXml, index);
+    }, [sampleXml]);
+
+    function handleRejection(checked) {
+        if (checked) {
             setNotificationBody({
                 kind: NotificationKinds.warning,
                 title: "Notification Message",
@@ -87,25 +80,29 @@ const Reject = (index, rejectionReasonsDisabled, setRejectionReasonsDisabled) =>
             });
             setNotificationVisible(true);
         }
+        setSampleXml({
+            ...sampleXml,
+            rejected: checked
+        });
         setRejectionReasonsDisabled(!rejectionReasonsDisabled);
     }
 
-    return (
-        <Checkbox labelText="" id={`reject_${index}`} onChange={(e) => handleRejection(e)}/>
-    );
-}
-export const TableSampleTableRows = (index, selectedSampleType, rejectSampleReasons, selectedTests,findConfigurationProperty, handleRemoveSampleTest) => {
-    const {id, name} = selectedSampleType;
-    const [rejectionReasonsDisabled, setRejectionReasonsDisabled] = useState(true);
     return [{
         select_radioButton: SampleSelect(index),
-        sampleType: Sequence(index),
+        sampleType: <CustomTextInput id={"sequence_" + index} defaultValue={index + 1}
+                                     onChange={null}/>,
         name: Sample_Type(index, name),
-        collectionDate: CollectionDate(index,findConfigurationProperty("currentDateAsText")),
-        collectionTime: CollectionTime(index, findConfigurationProperty("currentTimeAsText")),
-        collector: Collector(index),
-        reject: Reject(index, rejectionReasonsDisabled, setRejectionReasonsDisabled),
-        reason: RejectReasons(index, rejectSampleReasons, rejectionReasonsDisabled),
+        collectionDate: <CustomDatePicker id={"collectionDate_" + index}
+                                          onChange={(date) => handleCollectionDate(date)} labelText={""}/>,
+        collectionTime: <CustomTimePicker id={"collectionDate_" + index}
+                                          onChange={(time) => handleCollectionTime(time)}/>,
+        collector: <CustomTextInput id={"collector_" + id} onChange={(value) => handleCollector(value)}
+                                    defaultValue={""}/>,
+        reject: <CustomCheckBox id={"reject_" + index} onChange={(value) => handleRejection(value)} label=""/>,
+        reason: <CustomSelect id={"rejectedReasonId_" + index} options={rejectSampleReasons}
+                              disabled={rejectionReasonsDisabled}
+                              defaultSelect={defaultSelect}
+                              onChange={(e) => handleReasons(e)}/>,
         removeSample: RemoveSample(index, handleRemoveSampleTest)
     }];
 }
