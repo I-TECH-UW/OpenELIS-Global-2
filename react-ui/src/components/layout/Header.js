576d7935ace1c0e2585c4ce44fc740fe6397a7bc
CalebSLane <c.steelelane@gmail.com>
2023-05-22 13:32:18 -0700

    movr headers to side nav

--- react-ui/src/components/layout/Header.js
+++ react-ui/src/components/layout/Header.js
@@ -99,56 +99,7 @@ class OEHeader extends React.Component {
               </HeaderName>
               {this.props.isLoggedIn() && true && (
                 <>
-                  <HeaderNavigation aria-label="nav">
-                    <HeaderMenu aria-label="Order" menuLinkName="Order">
-                      <HeaderMenuItem href="/AddOrder">Add Order</HeaderMenuItem>
-                      <HeaderMenuItem href={config.serverBaseUrl +"/SampleEdit?type=readwrite"}>Modify Order</HeaderMenuItem>
-                      <HeaderMenuItem href={config.serverBaseUrl +"/ElectronicOrders"}>Incoming Orders</HeaderMenuItem>
-                      <HeaderMenuItem href={config.serverBaseUrl +"/SampleBatchEntrySetup"}>Batch Order Entry</HeaderMenuItem>
-                      <HeaderMenuItem href={config.serverBaseUrl +"/PrintBarcode"}>Barcode</HeaderMenuItem>
-                    </HeaderMenu>
-
-                    {this.props.isLoggedIn() && true && (
-                    <HeaderMenu aria-label="Patient" menuLinkName="Patient">
-                      <HeaderMenuItem href="/PatientManagement">Add/Edit Patient</HeaderMenuItem>
-                      <HeaderMenuItem href="/PatientHistory">Patient History</HeaderMenuItem>
-                    </HeaderMenu>
-                    )}
-                    <HeaderMenu
-                      aria-label="Non-Conforming Events"
-                      menuLinkName="Non-Conform"
-                    >
-                      <HeaderMenuItem href={config.serverBaseUrl + "/ReportNonConformingEvent"}>Report Non-Conforming Event</HeaderMenuItem>
-                      <HeaderMenuItem href={config.serverBaseUrl + "/ViewNonConformingEvent"}>View New Non-Conforming Events</HeaderMenuItem>
-                      <HeaderMenuItem href={config.serverBaseUrl + "/NCECorrectiveAction"}>Corrective actions</HeaderMenuItem>
-                    </HeaderMenu>
-                    <HeaderMenu aria-label="Workplan" menuLinkName="Workplan">
-                      <HeaderMenuItem href={config.serverBaseUrl + "/WorkPlanByTest?type=test"}>By Test Type</HeaderMenuItem>
-                      <HeaderMenuItem href={config.serverBaseUrl + "/WorkPlanByPanel?type=panel"}>By Panel Type</HeaderMenuItem>
-                      <HeaderMenuItem href={config.serverBaseUrl + "/WorkPlanByTestSection?type="}>By Unit</HeaderMenuItem>
-                      <HeaderMenuItem href={config.serverBaseUrl + "/WorkPlanByPriority?type=priority"}>By Priority</HeaderMenuItem>
-                    </HeaderMenu>
-                    <HeaderMenu aria-label="Results" menuLinkName="Results">
-                      <HeaderMenuItem href={config.serverBaseUrl + "/LogbookResults?type="}>Enter by Unit</HeaderMenuItem>
-                      <HeaderMenuItem href="/result">Search</HeaderMenuItem>
-                      <HeaderMenuItem href={config.serverBaseUrl + "/ReferredOutTests"}>Referred Tests</HeaderMenuItem>
-                    </HeaderMenu>
-                    <HeaderMenu
-                      aria-label="Validation" menuLinkName="Validation"                    >
-                      <HeaderMenuItem href={config.serverBaseUrl + "/ResultValidation?type=&test="}>Routine</HeaderMenuItem>
-                      <HeaderMenuItem href="/validationStudy">Study</HeaderMenuItem>
-                      <HeaderMenuItem href="/validation">Search</HeaderMenuItem>
-                    </HeaderMenu>
-
-                    <HeaderMenu aria-label="Reports" menuLinkName="Reports">
-                      <HeaderMenuItem href="/RoutineReports">Routine</HeaderMenuItem>
-                      <HeaderMenuItem href="/StudyReports">Study</HeaderMenuItem>
-                    </HeaderMenu>
-                    <HeaderMenuItem href="/admin">Admin</HeaderMenuItem>
-                  </HeaderNavigation>
-
-                  <HeaderMenuItem target="_blank" href={"http://ozone.uwdigi.org:8069/"}><FormattedMessage id="admin.billing"/></HeaderMenuItem>
-
+                    
                   {/* <HeaderMenuItem target="_blank" href={config.serverBaseUrl + "/MasterListsPage"}><FormattedMessage id="admin.billing"/></HeaderMenuItem> */}
 
                 </>
@@ -223,49 +174,49 @@ class OEHeader extends React.Component {
             <SideNavItems>
             <SideNavMenu aria-label="Order" title="Order">
                       <SideNavMenuItem href="/AddOrder">Add Order</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Modify Order</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Incoming Orders</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Batch Order Entry</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Barcode</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl +"/SampleEdit?type=readwrite"}>Modify Order</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl +"/ElectronicOrders"}>Incoming Orders</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl +"/SampleBatchEntrySetup"}>Batch Order Entry</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl +"/PrintBarcode"}>Barcode</SideNavMenuItem>
                     </SideNavMenu>
                     <SideNavMenu aria-label="Patient" title="Patient">
                       <SideNavMenuItem href="/PatientManagement">Add/Edit Patient</SideNavMenuItem>
                       <SideNavMenuItem href="/PatientHistory">Patient History</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Sub-link 3</SideNavMenuItem>
                     </SideNavMenu>
                     <SideNavMenu
                       aria-label="Non-Conforming Events"
                       title="Non-Conform"
                     >
-                      <SideNavMenuItem href="#">Sub-link 1</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Sub-link 2</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Sub-link 3</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl + "/ReportNonConformingEvent"}>Report Non-Conforming Event</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl + "/ViewNonConformingEvent"}>View New Non-Conforming Events</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl + "/NCECorrectiveAction"}>Corrective actions</SideNavMenuItem>
                     </SideNavMenu>
                     <SideNavMenu aria-label="Workplan" title="Workplan">
-                      <SideNavMenuItem href="#">Sub-link 1</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Sub-link 2</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Sub-link 3</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl + "/WorkPlanByTest?type=test"}>By Test Type</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl + "/WorkPlanByPanel?type=panel"}>By Panel Type</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl + "/WorkPlanByTestSection?type="}>By Unit</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl + "/WorkPlanByPriority?type=priority"}>By Priority</SideNavMenuItem>
                     </SideNavMenu>
                     <SideNavMenu aria-label="Results" title="Results">
-                      <SideNavMenuItem href="#">Sub-link 1</SideNavMenuItem>
-                      <SideNavMenuItem href="/result">Result Search</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Sub-link 3</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl + "/LogbookResults?type="}>Enter by Unit</SideNavMenuItem>
+                      <SideNavMenuItem href="/result">Search</SideNavMenuItem>
+                      <SideNavMenuItem href={config.serverBaseUrl + "/ReferredOutTests"}>Referred Tests</SideNavMenuItem>
                     </SideNavMenu>
                     <SideNavMenu
-                      aria-label="Validation"
-                      title="Validation"
-                    >
-                      <SideNavMenuItem href="#">Sub-link 1</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Sub-link 2</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Sub-link 3</SideNavMenuItem>
+                      aria-label="Validation" title="Validation"                    >
+                      <SideNavMenuItem href={config.serverBaseUrl + "/ResultValidation?type=&test="}>Routine</SideNavMenuItem>
+                      <SideNavMenuItem href="/validationStudy">Study</SideNavMenuItem>
+                      <SideNavMenuItem href="/validation">Search</SideNavMenuItem>
                     </SideNavMenu>
+
                     <SideNavMenu aria-label="Reports" title="Reports">
-                      <SideNavMenuItem href="#">Sub-link 1</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Sub-link 2</SideNavMenuItem>
-                      <SideNavMenuItem href="#">Sub-link 3</SideNavMenuItem>
+                      <SideNavMenuItem href="/RoutineReports">Routine</SideNavMenuItem>
+                      <SideNavMenuItem href="/StudyReports">Study</SideNavMenuItem>
                     </SideNavMenu>
                     <SideNavMenuItem href="/admin">Admin</SideNavMenuItem>
-                    <SideNavMenuItem target="_blank" href={"http://ozone.uwdigi.org:8069/"}><FormattedMessage id="admin.billing"/></SideNavMenuItem>
+
+                  <SideNavMenuItem target="_blank" href={"http://ozone.uwdigi.org:8069/"}><FormattedMessage id="admin.billing"/></SideNavMenuItem>
+
           
                        </SideNavItems>
           </SideNav>
