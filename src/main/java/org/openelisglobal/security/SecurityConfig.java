0fabe5d31b44634f35861c4363f6f390c44b76c5
CalebSLane <c.steelelane@gmail.com>
2023-07-18 14:42:03 -0700

    add better healthchek for OE

--- src/main/java/org/openelisglobal/security/SecurityConfig.java
+++ src/main/java/org/openelisglobal/security/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     // TODO should we move these to the properties files?
     // pages that have special security constraints
     public static final String[] OPEN_PAGES = { "/pluginServlet/**", "/ChangePasswordLogin",
-            "/UpdateLoginChangePassword" };
+            "/UpdateLoginChangePassword", "/health/**" };
     public static final String[] LOGIN_PAGES = { "/LoginPage", "/ValidateLogin" };
     public static final String[] AUTH_OPEN_PAGES = { "/Home", "/Dashboard", "/Logout", "/MasterListsPage",
             "/analyzer/runAction/**" };
