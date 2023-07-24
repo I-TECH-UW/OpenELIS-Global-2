732215e5d02c71c4fb4a9811bf6e32b95389c34d
abertnamanya <abertnamanya@gmail.com>
2023-07-24 16:27:32 +0300

    update qa github actions

--- react-ui/cypress.config.js
+++ react-ui/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://host.docker.internal",
+    baseUrl: "https://localhost",
     testIsolation: false,
   },
 });
