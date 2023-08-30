ad7d0c1b72dcbca95d2a1b05bdcfdd00e56de36b
CalebSLane <c.steelelane@gmail.com>
2023-08-30 16:56:00 -0700

    fix premature react fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
