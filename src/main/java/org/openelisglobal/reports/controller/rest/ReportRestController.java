91757b9ed7cb06b6b427b86f0c88b4211a7aeaba
CalebSLane <c.steelelane@gmail.com>
2023-10-30 16:11:34 -0700

    fix missing import

--- src/main/java/org/openelisglobal/reports/controller/rest/ReportRestController.java
+++ src/main/java/org/openelisglobal/reports/controller/rest/ReportRestController.java
@@ -25,6 +25,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.lowagie.text.DocumentException;
 
