1449edada6dd7e39d8b49897c0e050ef696bcd2c
Mutesasira Moses <mozzymutesa@gmail.com>
2024-01-08 22:21:49 +0300

    fix programs not loading for non admin users

--- frontend/src/components/addOrder/OrderEntryAdditionalQuestions.js
+++ frontend/src/components/addOrder/OrderEntryAdditionalQuestions.js
@@ -348,7 +348,7 @@ const OrderEntryAdditionalQuestions = ({
         },
       });
       getFromOpenElisServer(
-        "/program/" + event.target.value + "/questionnaire",
+        "/rest/program/" + event.target.value + "/questionnaire",
         (res) => setAdditionalQuestions(res, event),
       );
     }
