e1c85689f7c4de2976cf3bef53c2ea356ed3d186
Agaba-Derrick <agabaderrick18@gmail.com>
2024-01-12 07:27:20 -0500

    Add the right hierarchy of Breadcrumbs to pages

--- frontend/src/components/addOrder/Index.js
+++ frontend/src/components/addOrder/Index.js
@@ -690,6 +690,20 @@ const Index = () => {
 
   return (
     <>
+          <Grid fullWidth={true}>
+        <Column lg={16}>
+          <Breadcrumb>
+            <BreadcrumbItem href="/">
+              {intl.formatMessage({ id: "home.label" })}
+            </BreadcrumbItem>
+            <BreadcrumbItem href="/SamplePatientEntry">
+              {intl.formatMessage({
+                id: "SamplePatientEntry.label",
+              })}
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </Column>
+      </Grid>
       <Stack gap={10}>
         <div className="pageContent">
           {notificationVisible === true ? <AlertDialog /> : ""}
