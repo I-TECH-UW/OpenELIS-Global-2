f4cd086c590dfe3d52f373fd835366fe376a3ede
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2024-03-23 23:41:52 +0530

    rebase 01 + RestController fix

---
+++ src/main/java/org/openelisglobal/referral/controller/rest/ReferredOutTestsRestController.java
@@ -0,0 +1,59 @@
+package org.openelisglobal.referral.controller;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import javax.validation.Valid;
+
+import org.openelisglobal.common.controller.BaseController;
+import org.openelisglobal.common.services.DisplayListService;
+import org.openelisglobal.common.services.DisplayListService.ListType;
+import org.openelisglobal.common.util.IdValuePair;
+import org.openelisglobal.referral.form.ReferredOutTestsForm;
+import org.openelisglobal.referral.service.ReferralService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rest/")
+public class ReferredOutTestsRestController {
+
+    @Autowired
+    private ReferralService referralService;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setAllowedFields("labNumber", "testIds", "testUnitIds", "endDate",
+            "startDate", "dateType", "searchType", "selPatient");
+    }
+    
+    private void setupPageForDisplay(ReferredOutTestsForm form) {
+        if (form.getSearchType() != null) {
+            form.setReferralDisplayItems(referralService.getReferralItems(form));
+            form.setSearchFinished(true);
+        }
+        form.setTestSelectionList(DisplayListService.getInstance().getList(DisplayListService.ListType.ALL_TESTS));
+        form.setTestUnitSelectionList(DisplayListService.getInstance().getList(DisplayListService.ListType.TEST_SECTION_BY_NAME));
+    }
+
+    @GetMapping(value = "/ReferredOutTests", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ReferredOutTestsForm showReferredOutTests(@Valid ReferredOutTestsForm form) {
+        setupPageForDisplay(form);
+        return form;
+    }
+
+    @PostMapping(value = "/ReferredOutTests", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ReferredOutTestsForm submitReferredOutTests(@Valid @RequestBody ReferredOutTestsForm form) {
+        setupPageForDisplay(form);
+        return form;
+    }
+    
+    public class NonNumericTests {
+        public String testId;
+        public String testType;
+        public List<IdValuePair> dictionaryValues;
+    }
+
+}
