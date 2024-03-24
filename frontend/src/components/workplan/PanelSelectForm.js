522f0f9585cf6571c475a38b1125439bff5a2327
Ankit Matth <ankitmatth111@gmail.com>
2024-03-24 06:30:21 +0530

    Adding Placeholder Text in Workplan Dropdowns

--- frontend/src/components/workplan/PanelSelectForm.js
+++ frontend/src/components/workplan/PanelSelectForm.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Column, Grid, Select, SelectItem } from "@carbon/react";
-import { FormattedMessage, injectIntl } from "react-intl";
+import { FormattedMessage, injectIntl, useIntl } from "react-intl";
 import "../Style.css";
 import { getFromOpenElisServer } from "../utils/Utils";
 
@@ -20,13 +20,15 @@ function PanelSelectForm(props) {
     }
   };
 
+  const intl = useIntl();
+
   useEffect(() => {
     mounted.current = true;
     let panelId = new URLSearchParams(window.location.search).get("panelId");
     panelId = panelId ? panelId : "";
     getFromOpenElisServer("/rest/panels", (fetchedPanels) => {
       let panel = fetchedPanels.find((panel) => panel.id === panelId);
-      let panelLabel = panel ? panel.value : "";
+      let panelLabel = panel ? panel.value : intl.formatMessage({id:"input.placeholder.selectPanel"});
       setDefaultPanelId(panelId);
       setDefaultPanelLabel(panelLabel);
       props.value(panelId, panelLabel);
