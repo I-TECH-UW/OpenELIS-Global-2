f0aa352343807d206afe3bdd6df886fef0297e89
abert namanya <abertnamanya@gmail.com>
2024-04-02 09:24:26 +0000

    Cypress github Action updates

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://openelis28.openelis-global.org",
+    baseUrl: "https://localhost",
     testIsolation: false,
   },
 });
