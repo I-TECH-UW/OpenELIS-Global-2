e32558235e4ec323afbb61c249187a0b6185fa4f
Ankit Matth <ankitmatth111@gmail.com>
2024-04-06 08:23:41 +0530

    Removing unnecessary fetch requests.

--- frontend/cypress/support/e2e.js
+++ frontend/cypress/support/e2e.js
@@ -19,3 +19,10 @@ import "./commands";
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 require("cypress-xpath");
+const app = window.top;
+if (!app.document.head.querySelector("[data-hide-command-log-request]")) {
+    const style = app.document.createElement("style");
+    style.innerHTML = ".command-name-request, .command-name-xhr {display: none}";
+    style.setAttribute("data-hide-command-log-request", "");
+    app.document.head.appendChild(style);
+}
