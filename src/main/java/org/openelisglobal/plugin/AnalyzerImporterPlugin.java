370db94f6970159c31e17220b8ffa71ec0ae20a4
CalebSLane <c.steelelane@gmail.com>
2024-04-11 16:19:11 -0700

    expand capabilities of astm response

--- src/main/java/org/openelisglobal/plugin/AnalyzerImporterPlugin.java
+++ src/main/java/org/openelisglobal/plugin/AnalyzerImporterPlugin.java
@@ -19,9 +19,16 @@ package org.openelisglobal.plugin;
 import java.util.List;
 
 import org.openelisglobal.analyzerimport.analyzerreaders.AnalyzerLineInserter;
+import org.openelisglobal.analyzerimport.analyzerreaders.AnalyzerResponder;
 
 public interface AnalyzerImporterPlugin extends APlugin {
     public boolean isTargetAnalyzer(List<String> lines);
 
+    public boolean isAnalyzerResult(List<String> lines);
+
     public AnalyzerLineInserter getAnalyzerLineInserter();
+
+    default AnalyzerResponder getAnalyzerResponder() {
+        return null;
+    }
 }
