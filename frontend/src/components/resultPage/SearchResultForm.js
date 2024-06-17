4079edc1f41c53b3591d209889c50788f8cd436b
mozzy11 <mozzymutesa@gmail.com>
2024-06-17 20:03:40 +0300

    fix numeric result ui Validation

--- frontend/src/components/resultPage/SearchResultForm.js
+++ frontend/src/components/resultPage/SearchResultForm.js
@@ -1074,7 +1074,7 @@ export function SearchResults(props) {
                 id={"ResultValue" + row.id}
                 name={"testResult[" + row.id + "].resultValue"}
                 labelText=""
-                //type="number"
+                type="number"
                 value={row.resultValue}
                 style={validationState[row.id]?.style}
                 onMouseOut={(e) => {
@@ -1085,7 +1085,8 @@ export function SearchResults(props) {
                   let newValidationState = { ...validationState };
                   let validation = (newValidationState[row.id] =
                     validateNumericResults(value, row));
-                  e.target.value = validation.newValue;
+                    //e.target.value = validation.newValue;
+                    row.resultValue = validation.newValue
                   validation.style = {
                     ...validation?.style,
                     borderColor: validation.isCritical
