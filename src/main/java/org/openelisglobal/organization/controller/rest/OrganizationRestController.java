acb81caf63eff818ac4ea82624622ae7c9b9cec2
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2024-06-21 15:16:50 +0530

    CI build fix

--- src/main/java/org/openelisglobal/organization/controller/rest/OrganizationRestController.java
+++ src/main/java/org/openelisglobal/organization/controller/rest/OrganizationRestController.java
@@ -350,14 +350,14 @@ public class OrganizationRestController extends BaseController {
   public ResponseEntity<List<IdValuePair>> getParentOrganization() {
     List<Organization> parentOrgs = organizationService.getAllOrganizations();
     List<IdValuePair> idValuePairs = new ArrayList<>();
-    
+
     for (Organization parentOrg : parentOrgs) {
-        IdValuePair idValuePair = new IdValuePair(parentOrg.getId(), parentOrg.getOrganizationName());
-        idValuePairs.add(idValuePair);
+      IdValuePair idValuePair = new IdValuePair(parentOrg.getId(), parentOrg.getOrganizationName());
+      idValuePairs.add(idValuePair);
     }
 
     return ResponseEntity.ok(idValuePairs);
-}
+  }
 
   @PostMapping(value = "/Organization")
   public ResponseEntity<Object> showUpdateOrganization(
