d368180ec1e2199dbac0fd2d469a851a7b07a248
josephbate <sjb.lav@gmail.com>
2024-07-02 10:21:26 +0300

    Add more integration tests to the Patient and Person service

--- src/test/java/org/openelisglobal/patient/PatientServiceTest.java
+++ src/test/java/org/openelisglobal/patient/PatientServiceTest.java
@@ -5,7 +5,6 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,66 +22,82 @@ public class PatientServiceTest extends BaseWebContextSensitiveTest {
 
     @Autowired
     PersonService personService;
+    String patientId;
 
     @Before
     public void init() throws Exception {
-        patientService.deleteAll(patientService.getAll());
-        personService.deleteAll(personService.getAll());
-    }
-
-    @After
-    public void tearDown() {
-        patientService.deleteAll(patientService.getAll());
-        personService.deleteAll(personService.getAll());
+      String firstName = "John";
+      String lastname = "Doe";
+      String dob = "12/12/1992";
+      String gender = "M";
+      String nId = "NID45";
+      String race = "Black";
+      Patient pat = createPatient(firstName, lastname, dob, gender, race, nId);
+  
+      // save patient to the DB
+      patientId = patientService.insert(pat);
+      
     }
 
     @Test
     public void createPatient_shouldCreateNewPatient() throws Exception {
-        String firstName = "John";
-        String lastname = "Doe";
-        String dob = "12/12/1992";
-        String gender = "M";
-        Patient pat = createPatient(firstName, lastname, dob, gender);
-
-        Assert.assertEquals(0, patientService.getAllPatients().size());
-        // save patient to the DB
-        String patientId = patientService.insert(pat);
         Patient savedPatient = patientService.get(patientId);
 
         Assert.assertEquals(1, patientService.getAllPatients().size());
-        Assert.assertEquals(firstName, savedPatient.getPerson().getFirstName());
-        Assert.assertEquals(lastname, savedPatient.getPerson().getLastName());
-        Assert.assertEquals(gender, savedPatient.getGender());
+        Assert.assertEquals("John", savedPatient.getPerson().getFirstName());
+        Assert.assertEquals("Doe", savedPatient.getPerson().getLastName());
+        Assert.assertEquals("M", savedPatient.getGender());
     }
 
     @Test
     public void getAllPatients_shouldGetAllPatients() throws Exception {
-        String firstName = "John";
-        String lastname = "Doe";
-        String dob = "12/12/1992";
-        String gender = "M";
-        Patient patient = createPatient(firstName, lastname, dob, gender);
-        patientService.insert(patient);
         Assert.assertEquals(1, patientService.getAllPatients().size());
     }
 
-    private Patient createPatient(String firstName, String LastName, String birthDate, String gender)
-            throws ParseException {
-        Person person = new Person();
-        person.setFirstName(firstName);
-        person.setLastName(LastName);
-        personService.save(person);
 
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        Date date = dateFormat.parse(birthDate);
-        long time = date.getTime();
-        Timestamp dob = new Timestamp(time);
+    @Test
+    public void getNationalId_shouldReturnNationalId() throws Exception{
+        Patient savedPatient = patientService.get(patientId);
 
-        Patient pat = new Patient();
-        pat.setPerson(person);
-        pat.setBirthDate(dob);
-        pat.setGender(gender);
+        Assert.assertEquals("NID45", savedPatient.getNationalId());
 
-        return pat;
     }
+    @Test
+    public void getPatientByNationalId_shouldGetAllPatientsByNationalId(){
+        String nId = "NID45";
+
+        Assert.assertEquals("John", patientService.getPatientByNationalId(nId).getPerson().getFirstName());
+
+    }
+
+    @Test
+    public void getPatientsByNationalId_shouldGetAllPatientsByNationalId(){
+        String nId = "NID45";
+
+        Assert.assertEquals(1, patientService.getPatientsByNationalId(nId).size());
+
+    }
+
+    private Patient createPatient(String firstName, String LastName, String birthDate, String gender, String race, String nId)
+    throws ParseException {
+  Person person = new Person();
+  person.setFirstName(firstName);
+  person.setLastName(LastName);
+  personService.save(person);
+
+  DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+  Date date = dateFormat.parse(birthDate);
+  long time = date.getTime();
+  Timestamp dob = new Timestamp(time);
+
+  Patient pat = new Patient();
+  pat.setPerson(person);
+  pat.setBirthDate(dob);
+  pat.setGender(gender);
+  pat.setRace(race);
+  pat.setNationalId(nId);
+
+  return pat;
+}
+
 }
