13758e46498275e088ccc1f85549c23d8ee6d61c
manishjha-04 <stayconnectedwithmanish@gmail.com>
2024-07-03 15:52:16 +0530

    fix formatting

--- src/main/java/org/openelisglobal/testconfiguration/controller/rest/TestCatalogRestController.java
+++ src/main/java/org/openelisglobal/testconfiguration/controller/rest/TestCatalogRestController.java
@@ -25,13 +25,12 @@ import org.openelisglobal.testresult.valueholder.TestResult;
 import org.openelisglobal.typeofsample.valueholder.TypeOfSample;
 import org.openelisglobal.typeoftestresult.service.TypeOfTestResultServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.MediaType;
-
 
 @RestController
 @RequestMapping("/rest")
