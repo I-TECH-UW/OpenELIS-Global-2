3fed17319d78a80a423f2ecad75eacd76061eb63
manishjha-04 <stayconnectedwithmanish@gmail.com>
2024-07-12 16:21:38 +0530

    E2E coverage for View Nce and Corrective Actions

--- frontend/cypress/e2e/nonConform.cy.js
+++ frontend/cypress/e2e/nonConform.cy.js
@@ -70,17 +70,113 @@ describe("Report Non-Conforming Event", function () {
     });
 
     nonConform.getAndSaveNceNumber();
+    skipBeforeEach = true;
   });
 
   it("Should enter the details and Submit the NCE Reporting Form", function () {
-    skipBeforeEach = true; 
     cy.fixture("NonConform").then((nonConformData) => {
       nonConform.enterStartDate(nonConformData.dateOfEvent);
       nonConform.enterReportingUnit(nonConformData.reportingUnit);
       nonConform.enterDescription(nonConformData.description);
       nonConform.enterSuspectedCause(nonConformData.suspectedCause);
       nonConform.enterCorrectiveAction(nonConformData.proposedCorrectiveAction);
+      nonConform.submitFormNce();
+    });
+  });
+});
+
+describe("View New Non-Conforming Event", function () {
+  let skipBeforeEach = false;
+
+  beforeEach("navigate to View New Non-Conforming Event Page", function () {
+    if (!skipBeforeEach) {
+      homePage = loginPage.goToHomePage();
+      nonConform = homePage.goToViewNCE();
+    }
+  });
+
+  it("User visits View Non-Conforming Event Page", function () {
+    nonConform
+      .getViewNonConformTitle()
+      .should("contain.text", "View New Non Conform Event");
+  });
+
+  it("Should Search by Lab Number and Validate the results", function () {
+    cy.fixture("EnteredOrder").then((order) => {
+      nonConform.selectSearchType("Lab Number");
+      nonConform.enterSearchField(order.labNo);
+      nonConform.clickSearchButton();
+      nonConform.validateLabNoSearchResult(order.labNo);
+    });
+  });
+
+  it("Should Search by Lab Number and Validate the results", function () {
+    cy.fixture("NonConform").then((nce) => {
+      nonConform.selectSearchType("NCE Number");
+      nonConform.enterSearchField(nce.NceNumber);
+      nonConform.clickSearchButton();
+      nonConform.validateNCESearchResult(nce.NceNumber);
+    });
+    skipBeforeEach = true;
+  });
+
+  it("Should Enter The details and Submit it", function () {
+    cy.fixture("NonConform").then((nce) => {
+      nonConform.enterNceCategory(nce.nceCategory);
+      nonConform.enterNceType(nce.nceType);
+      nonConform.enterConsequences(nce.consequences);
+      nonConform.enterRecurrence(nce.recurrence);
+      nonConform.enterLabComponent(nce.labComponent);
+      nonConform.enterDescriptionAndComments(nce.test);
       nonConform.submitForm();
+    });
+  });
+});
+
+describe("Corrective Actions", function () {
+  let skipBeforeEach = false;
+
+  beforeEach("navigate Corrective Action Page", function () {
+    if (!skipBeforeEach) {
+      homePage = loginPage.goToHomePage();
+      nonConform = homePage.goToCorrectiveActions();
+    }
+  });
+
+  it("User visits Corrective Actions Page", function () {
+    nonConform
+      .getViewNonConformTitle()
+      .should("contain.text", "Nonconforming Events Corrective Action");
+  });
+
+  it("Should Search by Lab Number and Validate the results", function () {
+    cy.fixture("EnteredOrder").then((order) => {
+      nonConform.selectSearchType("Lab Number");
+      nonConform.enterSearchField(order.labNo);
+      nonConform.clickSearchButton();
+      nonConform.validateLabNoSearchResultCorective(order.labNo);
+    });
+  });
+
+  it("Should Search by NCE Number and Validate the results", function () {
+    cy.fixture("NonConform").then((nce) => {
+      nonConform.selectSearchType("NCE Number");
+      nonConform.enterSearchField(nce.NceNumber);
+      nonConform.clickSearchButton();
+      nonConform.validateNCESearchResult(nce.NceNumber);
+    });
+    skipBeforeEach = true;
+  });
+
+  it("Should enter the discussion details and submit", function () {
+    cy.fixture("NonConform").then((nce) => {
+      nonConform.enterDiscussionDate(nce.dateOfEvent);
+      nonConform.enterProposedCorrectiveAction(nce.proposedCorrectiveAction);
+      nonConform.enterDateCompleted(nce.dateOfEvent);
+      nonConform.selectActionType();
+      nonConform.selectResolution();
+      nonConform.enterDateCompleted0(nce.dateOfEvent);
+      nonConform.clickSubmitButton();
     });
   });
 });
--- frontend/cypress/pages/NonConformPage.js
+++ frontend/cypress/pages/NonConformPage.js
@@ -3,6 +3,10 @@ class NonConform {
     return cy.get("h2");
   }
 
+  getViewNonConformTitle(){
+    return cy.get("h2");
+  }
+
   selectSearchType(type) {
     cy.get("#type").select(type);
   }
@@ -19,6 +23,18 @@ class NonConform {
     cy.get("tbody > tr > :nth-child(2)")
       .invoke("text")
       .should("eq", expectedValue);
+  }
+
+  validateLabNoSearchResult(labNo){
+    cy.get('.orderLegendBody > :nth-child(2) > :nth-child(7) > :nth-child(2)').invoke('text').should('eq', labNo);
+  }
+
+  validateNCESearchResult(NCENo){
+    cy.get('[style="margin-bottom: 10px; color: rgb(85, 85, 85);"]').invoke('text').should('eq', NCENo);
+  }
+
+  validateLabNoSearchResultCorective(labNo){
+    cy.get('.cds--subgrid > :nth-child(7) > :nth-child(2)').invoke('text').should('eq', labNo);
   }
 
   clickCheckbox() {
@@ -39,19 +55,77 @@ class NonConform {
 
   enterDescription(description) {
     cy.get('#text-area-1').type(description);
-  
   }
+
   enterSuspectedCause(SuspectedCause) {
     cy.get('#text-area-2').type(SuspectedCause);
-  
   }
+
   enterCorrectiveAction(correctiveaction) {
     cy.get('#text-area-3').type(correctiveaction);
-  
+  }
+
+  enterNceCategory(nceCategory) {
+    cy.get('#nceCategory').select(nceCategory);
+  }
+
+  enterNceType(nceType) {
+    cy.get('#nceType').select(nceType);
+  }
+
+  enterConsequences(consequences) {
+    cy.get('#consequences').select(consequences);
+  }
+
+  enterRecurrence(recurrence) {
+    cy.get('#recurrence').select(recurrence);
+  }
+
+  enterLabComponent(labComponent) {
+    cy.get('#labComponent').select(labComponent);
+  }
+
+  enterDescriptionAndComments(testText) {
+    cy.get('#text-area-10').type(testText);
+    cy.get('#text-area-3').type(testText);
+    cy.get('#text-area-2').type(testText);
   }
 
   submitForm() {
-    cy.get(":nth-child(14) > .cds--btn").click();
+    cy.get(':nth-child(28) > .cds--btn').click();
+  }
+
+  submitFormNce(){
+    cy.get(':nth-child(14) > .cds--btn').click();
+  }
+
+
+  enterDiscussionDate(date) {
+    cy.get('.cds--date-picker-input__wrapper > #tdiscussionDate').type(date);
+  }
+  
+  enterProposedCorrectiveAction(action) {
+    cy.get('#text-area-corrective').type(action, { force: true });
+  }
+  
+  enterDateCompleted(date) {
+    cy.get('.cds--date-picker-input__wrapper > #dateCompleted').type(date);
+  }
+  
+  selectActionType() {
+    cy.get(':nth-child(1) > .cds--checkbox-label').click();
+  }
+  
+  selectResolution() {
+    cy.get(':nth-child(1) > .cds--radio-button__label').click();
+  }
+  
+  enterDateCompleted0(date) {
+    cy.get('.cds--date-picker-input__wrapper > #dateCompleted-0').type(date);
+  }
+  
+  clickSubmitButton() {
+    cy.get(':nth-child(38) > .cds--btn').click();
   }
 
   getAndSaveNceNumber() {
