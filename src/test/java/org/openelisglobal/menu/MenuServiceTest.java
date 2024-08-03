954f280e95a52ffac24751aaef0b30f39c4050d3
Agaba Derrick <agabaderrick18@gmail.com>
2024-08-03 22:16:17 +0300

    Add-More-menu-service-tests

--- src/test/java/org/openelisglobal/menu/MenuServiceTest.java
+++ src/test/java/org/openelisglobal/menu/MenuServiceTest.java
@@ -1,16 +1,19 @@
 package org.openelisglobal.menu;
 
-import org.openelisglobal.BaseWebContextSensitiveTest;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openelisglobal.BaseWebContextSensitiveTest;
+import org.openelisglobal.menu.form.AdminMenuForm;
 import org.openelisglobal.menu.service.MenuService;
 import org.openelisglobal.menu.util.MenuItem;
+import org.openelisglobal.menu.valueholder.AdminMenuItem;
 import org.openelisglobal.menu.valueholder.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
-import java.util.List;
-
-import static org.junit.Assert.*;
 
 public class MenuServiceTest extends BaseWebContextSensitiveTest {
     @Autowired
@@ -18,12 +21,10 @@ public class MenuServiceTest extends BaseWebContextSensitiveTest {
 
     @Before
     public void setUp() {
-        // Setup code here if needed
     }
 
     @After
     public void tearDown() {
-        // Tear down code here if needed
     }
 
     @Test
@@ -89,6 +90,135 @@ public class MenuServiceTest extends BaseWebContextSensitiveTest {
         assertTrue(activeMenus.stream().allMatch(Menu::getIsActive));
     }
 
+    @Test
+    public void testGetSetAdminMenuItems() {
+        AdminMenuForm form = new AdminMenuForm();
 
+        List<AdminMenuItem> adminMenuItems = new ArrayList<>();
+        AdminMenuItem item1 = new AdminMenuItem();
+        item1.setPath("/path1");
+        AdminMenuItem item2 = new AdminMenuItem();
+        item2.setPath("/path2");
+        adminMenuItems.add(item1);
+        adminMenuItems.add(item2);
+
+        form.setAdminMenuItems(adminMenuItems);
+
+        List<AdminMenuItem> result = form.getAdminMenuItems();
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("/path1", result.get(0).getPath());
+        assertEquals("/path2", result.get(1).getPath());
+    }
+
+    @Test
+    public void testGetSetTotalRecordCount() {
+        AdminMenuForm form = new AdminMenuForm();
+        form.setTotalRecordCount("100");
+        assertEquals("100", form.getTotalRecordCount());
+    }
+
+    @Test
+    public void testGetSetFromRecordCount() {
+        AdminMenuForm form = new AdminMenuForm();
+        form.setFromRecordCount("10");
+        assertEquals("10", form.getFromRecordCount());
+    }
+
+    @Test
+    public void testGetSetToRecordCount() {
+        AdminMenuForm form = new AdminMenuForm();
+        form.setToRecordCount("20");
+        assertEquals("20", form.getToRecordCount());
+    }
+
+    @Test
+    public void testGetSetPresentationOrder() {
+        Menu menu = new Menu();
+        menu.setPresentationOrder(1);
+        assertEquals(1, menu.getPresentationOrder());
+    }
+
+    @Test
+    public void testGetSetElementId() {
+        Menu menu = new Menu();
+        menu.setElementId("elementId");
+        assertEquals("elementId", menu.getElementId());
+    }
+
+    @Test
+    public void testGetSetActionURL() {
+        Menu menu = new Menu();
+        menu.setActionURL("http://example.com");
+        assertEquals("http://example.com", menu.getActionURL());
+    }
+
+    @Test
+    public void testGetSetClickAction() {
+        Menu menu = new Menu();
+        menu.setClickAction("clickAction");
+        assertEquals("clickAction", menu.getClickAction());
+    }
+
+    @Test
+    public void testGetSetDisplayKey() {
+        Menu menu = new Menu();
+        menu.setDisplayKey("displayKey");
+        assertEquals("displayKey", menu.getDisplayKey());
+    }
+
+    @Test
+    public void testGetSetToolTipKey() {
+        Menu menu = new Menu();
+        menu.setToolTipKey("toolTipKey");
+        assertEquals("toolTipKey", menu.getToolTipKey());
+    }
+
+    @Test
+    public void testGetSetParent() {
+        Menu parentMenu = new Menu();
+        Menu childMenu = new Menu();
+        childMenu.setParent(parentMenu);
+        assertEquals(parentMenu, childMenu.getParent());
+    }
+
+    @Test
+    public void testGetLocalizedTitle() {
+        Menu menu = new Menu();
+        menu.setDisplayKey("testDisplayKey");
+
+        String title = menu.getLocalizedTitle();
+        assertNotNull(title);
+    }
+
+    @Test
+    public void testGetLocalizedTooltip() {
+        Menu menu = new Menu();
+        menu.setToolTipKey("testToolTipKey");
+
+        String tooltip = menu.getLocalizedTooltip();
+        assertNotNull(tooltip);
+
+    }
+
+    @Test
+    public void testSetGetOpenInNewWindow() {
+        Menu menu = new Menu();
+        menu.setOpenInNewWindow(true);
+        assertTrue(menu.isOpenInNewWindow());
+    }
+
+    @Test
+    public void testSetGetIsActive() {
+        Menu menu = new Menu();
+        menu.setIsActive(true);
+        assertTrue(menu.getIsActive());
+    }
+
+    @Test
+    public void testSetGetHideInOldUI() {
+        Menu menu = new Menu();
+        menu.setHideInOldUI(true);
+        assertTrue(menu.isHideInOldUI());
+    }
 }
-
