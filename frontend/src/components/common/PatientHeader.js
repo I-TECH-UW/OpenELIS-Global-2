6bb616d1986d470d7b00a7d6979ad0ceb50c43a8
Isabirye1515 <isabiryeelijah15@gmail.com>
2024-09-17 15:54:36 +0300

    update on the Header Sizing

--- frontend/src/components/common/PatientHeader.js
+++ frontend/src/components/common/PatientHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Column, Section, Tag } from "@carbon/react";
+import { Tag } from "@carbon/react";
 import { FormattedMessage } from "react-intl";
 import Avatar from "react-avatar";
 import GeoPattern from "geopattern";
@@ -24,150 +24,97 @@ const PatientHeader = (props) => {
     className = "patient-header",
   } = props;
   const patternUrl = GeoPattern.generate(id).toDataUri();
-  return (
-    <Grid fullWidth={true}>
-      <Column lg={16} md={8} sm={4}>
-        <Section>
-          <Section>
-            {id ? (
-              <div className={className}>
-                <Grid>
-                  <Column lg={4} md={2} sm={2}>
-                    <div
-                      className={
-                        referringFacility ? "patientAvatar2" : "patientAvatar"
-                      }
-                      role="img"
-                    >
-                      <Avatar
-                        alt={"Patient avatar"}
-                        color="rgba(0,0,0,0)"
-                        name={
-                          patientName ? patientName : lastName + " " + firstName
-                        }
-                        src={""}
-                        size={referringFacility ? "150" : "120"}
-                        textSizeRatio={2}
-                        style={{
-                          backgroundImage: `url(${patternUrl})`,
-                          backgroundRepeat: "round",
-                        }}
-                      />
-                    </div>
-                  </Column>
-                  <Column lg={10} md={5} sm={2}>
-                    <div className="patient-name">
-                      {patientName ? patientName : lastName + " " + firstName}
-                    </div>
-                    <div className="patient-dob">
-                      {" "}
-                      <Tag type="blue">
-                        <FormattedMessage id="patient.label.sex" /> :
-                      </Tag>
-                      {gender === "M" ? (
-                        <FormattedMessage id="patient.male" />
-                      ) : (
-                        <FormattedMessage id="patient.female" />
-                      )}{" "}
-                      <Tag type="blue">
-                        {age ? (
-                          <FormattedMessage id="patient.label.age" />
-                        ) : (
-                          <FormattedMessage id="patient.dob" />
-                        )}{" "}
-                        :
-                      </Tag>{" "}
-                      {age ? age : dob}
-                    </div>
-                    {nationalId && (
-                      <div className="patient-id">
-                        <Tag type="blue">
-                          <FormattedMessage id="patient.natioanalid" /> :
-                        </Tag>
-                        {nationalId}
-                      </div>
-                    )}
-                    {subjectNumber && (
-                      <div className="patient-id">
-                        <Tag type="blue">
-                          <FormattedMessage id="patient.subject.number" /> :
-                        </Tag>
-                        {subjectNumber}{" "}
-                      </div>
-                    )}
-                    {accesionNumber && (
-                      <div className="patient-id">
-                        <Tag type="blue">
-                          <FormattedMessage id="quick.entry.accession.number" />{" "}
-                          :
-                        </Tag>
-                        {accesionNumber}{" "}
-                      </div>
-                    )}
-                    {orderDate && (
-                      <div className="patient-id">
-                        <div className="patient-id">
-                          <Tag type="blue">
-                            <FormattedMessage id="sample.label.orderdate" /> :
-                          </Tag>
-                          {orderDate}
 
-                          <Tag type="blue">
-                            <FormattedMessage id="sample.label.requester" />: :
-                          </Tag>
-                          {requester}
-                        </div>
-                      </div>
-                    )}
-                    {referringFacility && (
-                      <div className="patient-id">
-                        <Tag type="blue">
-                          <FormattedMessage id="sample.label.facility" />:
-                        </Tag>
-                        {referringFacility}{" "}
-                        <Tag type="blue">
-                          <FormattedMessage id="sample.label.dept" /> :
-                        </Tag>{" "}
-                        {department}
-                      </div>
-                    )}
-                  </Column>
-                </Grid>
-              </div>
+  return (
+    <div className={className}>
+      <div className="avatar-container">
+        <Avatar
+          alt={"Patient avatar"}
+          color="rgba(0,0,0,0)"
+          name={patientName ? patientName : `${lastName} ${firstName}`}
+          src={""}
+          size={referringFacility ? "150" : "120"}
+          textSizeRatio={2}
+          style={{
+            backgroundImage: `url(${patternUrl})`,
+            backgroundRepeat: "round",
+          }}
+          className={referringFacility ? "patientAvatar2" : "patientAvatar"}
+        />
+      </div>
+      <div className="details-container">
+        <div className="patient-name">
+          {patientName ? patientName : `${lastName} ${firstName}`}
+        </div>
+        <div className="patient-dob">
+          <Tag className="tag" type="blue">
+            <FormattedMessage id="patient.label.sex" /> :
+          </Tag>
+          {gender === "M" ? (
+            <FormattedMessage id="patient.male" />
+          ) : (
+            <FormattedMessage id="patient.female" />
+          )}
+          <Tag className="tag" type="blue">
+            {age ? (
+              <FormattedMessage id="patient.label.age" />
             ) : (
-              <div className={className}>
-                <Grid>
-                  <Column lg={4} md={2} sm={2}>
-                    <Avatar
-                      alt={"Patient avatar"}
-                      color="rgba(0,0,0,0)"
-                      name={"!"}
-                      src={""}
-                      size={referringFacility ? "150" : "120"}
-                      textSizeRatio={2}
-                      style={{
-                        backgroundImage: `url(${patternUrl})`,
-                        backgroundRepeat: "round",
-                      }}
-                    />
-                  </Column>
-                  <Column lg={8}>
-                    <div className="patient-name">
-                      {" "}
-                      {isOrderPage ? (
-                        <FormattedMessage id="patient.label.nopatientid" />
-                      ) : (
-                        <FormattedMessage id="patient.label.nopatientid" />
-                      )}
-                    </div>
-                  </Column>
-                </Grid>
-              </div>
-            )}
-          </Section>
-        </Section>
-      </Column>
-    </Grid>
+              <FormattedMessage id="patient.dob" />
+            )}{" "}
+            :
+          </Tag>
+          {age ? age : dob}
+        </div>
+        {nationalId && (
+          <div className="patient-id">
+            <Tag className="tag" type="blue">
+              <FormattedMessage id="patient.natioanalid" /> :
+            </Tag>
+            {nationalId}
+          </div>
+        )}
+        {subjectNumber && (
+          <div className="patient-id">
+            <Tag className="tag" type="blue">
+              <FormattedMessage id="patient.subject.number" /> :
+            </Tag>
+            {subjectNumber}
+          </div>
+        )}
+        {accesionNumber && (
+          <div className="patient-id">
+            <Tag className="tag" type="blue">
+              <FormattedMessage id="quick.entry.accession.number" /> :
+            </Tag>
+            {accesionNumber}
+          </div>
+        )}
+        {orderDate && (
+          <div className="patient-id">
+            <Tag className="tag" type="blue">
+              <FormattedMessage id="sample.label.orderdate" /> :
+            </Tag>
+            {orderDate}
+            <Tag className="tag" type="blue">
+              <FormattedMessage id="sample.label.requester" /> :
+            </Tag>
+            {requester}
+          </div>
+        )}
+        {referringFacility && (
+          <div className="patient-id">
+            <Tag className="tag" type="blue">
+              <FormattedMessage id="sample.label.facility" /> :
+            </Tag>
+            {referringFacility}
+            <Tag className="tag" type="blue">
+              <FormattedMessage id="sample.label.dept" /> :
+            </Tag>
+            {department}
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
