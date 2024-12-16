23da9b0cfb6302793989cdb9e6bf38e8c35f8f62
Mutesasira Moses <mozzymutesa@gmail.com>
2024-12-16 11:42:08 +0300

    Update AppTestConfig.java

--- src/test/java/org/openelisglobal/AppTestConfig.java
+++ src/test/java/org/openelisglobal/AppTestConfig.java
@@ -179,12 +179,6 @@ public class AppTestConfig implements WebMvcConfigurer {
 
     @Bean()
     @Profile("test")
-    public CloseableHttpClient cityStateZipServiceloseableHttpClient() {
-        return mock(CloseableHttpClient.class);
-    }
-
-    @Bean()
-    @Profile("test")
     public ExternalConnectionService externalConnectService() {
         return mock(ExternalConnectionService.class);
     }
